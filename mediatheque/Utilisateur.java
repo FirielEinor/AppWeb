f935e2fae0778c773fe162a6cdcc57cfc05f00de
villemai <villemai@B-0-02-02.iutparis.local>
2018-02-23 10:43:49 +0100

    user

--- mediatheque/Utilisateur.java
+++ mediatheque/Utilisateur.java
@@ -1,5 +1,20 @@
 package mediatheque;
 
 public class Utilisateur {
+	private String login;
+	private String password;
 	
+	public Utilisateur(String login, String password) {
+		super();
+		this.login = login;
+		this.password = password;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
 }
