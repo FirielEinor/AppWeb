7de21b5a2dc2a08a7b7ba45ae506191e6d2a9fc5
patry <patry@B-0-13-04.iutparis.local>
2018-03-16 11:22:24 +0100

    aerazrzqsdf

--- services/ServletInit.java
+++ services/ServletInit.java
@@ -1,15 +1,16 @@
 package services;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 public class ServletInit extends HttpServlet {
 	@Override
-	public void init() throws ServletException {
+	public void init(ServletConfig arg0) throws ServletException {
 		// TODO Auto-generated method stub
-		super.init();
+		super.init(arg0);
 		try {
-			Class.forName("persistantdata.MediathequeData");
+			Class.forName(arg0.toString());
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
