019afbc4adfd596dcc292683b196f216c833f76c
patry <patry@B-1-05-13.iutparis.local>
2018-03-23 17:09:51 +0100

    azqfqsf

--- services/ConfirmRendre.java
+++ services/ConfirmRendre.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import mediatheque.Mediatheque;
 
@@ -15,8 +16,11 @@ public class ConfirmRendre extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
+		HttpSession session = request.getSession(false);
 		int numDoc = Integer.parseInt(request.getParameter("idDoc"));
 		Mediatheque m = Mediatheque.getInstance();
 		m.retour(m.getDocument(numDoc));
+		out.println("Document rendu !! <a href='http://localhost:8080/ProjectWebJava/service?login=" + session.getAttribute("login") + "&password=" + session.getAttribute("password")+"'>retour </a>");
+
 	}
 }
