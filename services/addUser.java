90aa15102af53cb7354a147ed645b8d42e27b614
villemai <villemai@B-1-05-12.iutparis.local>
2018-03-23 10:47:28 +0100

    tggrehe

--- services/addUser.java
+++ services/addUser.java
@@ -17,35 +17,9 @@ public class addUser extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
-        String login = request.getParameter("login");
-        String password = request.getParameter("mdp");
-        String type = request.getParameter("type");
-        String message = "";
+        String message = "Dans une prochaine version !";
         
-        if (login != null && password != null) {
-			Utilisateur utilisateur = new Utilisateur(login, password, type);
-			HttpSession sessionUtilisateur = request.getSession(true);
-			sessionUtilisateur.setAttribute("userId", utilisateur);
-			message = "<h1>Vous vous êtes identifié avec le login : " + login + "</h1>";
-		} else {
-			message = "<h1>Nouvel Utilisareur</h1>";
-		}
-        out.println("<html>");
-        	out.println("<body>");
-        		out.println(message);
-        		out.print("<form action= http://localhost:8080/ProjectWebJava/service ");
-                out.print("login\" ");
-                out.println("method=POST>");
-                out.println("Login:");
-                out.println("<input type=text size=20 name=login>");
-                out.println("<br>");
-                out.println("Password:");
-                out.println("<input type=text size=20 name=password>");
-                out.println("<br>");
-                out.println("<input type=submit>");
-                out.println("</form>");
-        	out.println("</body>");
-        out.println("</html>");
+        out.println("<h1>"+message+"</h1>");
     }
 
 }
